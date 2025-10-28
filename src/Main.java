afcc0f5f35957b212c719a1b8dda834a09d7583c
axian04 <42780774+alisalman0499@users.noreply.github.com>
2025-10-28 11:11:38 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,7 @@ public class Main {
 
         Game g = new Game("Yatzy",3);
         g.startSession();
+        System.out.println("Game started");
 
         // missing here is the logic of the game
 
