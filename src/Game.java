c19e23bbababa9de7a96d3eeeedbbe857f39d43d
stinetorndal <cph-sc95@stud.ek.dk>
2025-10-31 09:45:37 +0100

    sprint3

--- src/Game.java
+++ src/Game.java
@@ -14,40 +14,39 @@ public class Game {
     FileIO io = new FileIO();
     Player currentPlayer;
 
-    public Game(String name, int maxPlayers){
+    public Game(String name, int maxPlayers) {
         this.name = name;
         this.maxPlayers = maxPlayers;
         players = new ArrayList<>();
     }
 
 
-    public void startSession(){
+    public void startSession() {
         ui.displayMsg("Welcome to the game.");
         ArrayList<String> data = io.readData("data/playerData.csv");
-        if(!data.isEmpty() && ui.promptBinary("Continue previously saved game? Y/N")){
-            for(String s : data){
-                String[] values =  s.split(",");//  "tess, 0"
+        if (!data.isEmpty() && ui.promptBinary("Continue previously saved game? Y/N")) {
+            for (String s : data) {
+                String[] values = s.split(",");//  "tess, 0"
                 int score = Integer.parseInt(values[1].trim());
-                createPlayer(values[0],score);
+                createPlayer(values[0], score);
             }
 
-        }else{
+        } else {
             registerPlayers();
         }
         displayPlayers();
     }
 
 
-
-    public void registerPlayers(){
+    public void registerPlayers() {
         int totalPlayers = 1;
         ui.displayMsg("Hvor mange deltager?");
 
-        while(totalPlayers < 2 || totalPlayers > 6){
-            totalPlayers = ui.promptNumeric("Tast et tal (min 2 og max "+this.maxPlayers+")");
+        while (totalPlayers < 2 || totalPlayers > 6) {
+            totalPlayers = ui.promptNumeric("Tast et tal (min 2 og max " + this.maxPlayers + ")");
         }
 
-        while(players.size() < totalPlayers) {
+        while (players.size() < totalPlayers) {
             String playerName = ui.promptText("Tast spiller navn");
             this.createPlayer(playerName, 0);
         }
@@ -56,52 +55,70 @@ public class Game {
     }
 
 
-    private void createPlayer(String name, int score){
+    private void createPlayer(String name, int score) {
         Player p = new Player(name, score);
         players.add(p);
     }
-    public void displayPlayers(){
-        for(Player p:players){
+
+    public void displayPlayers() {
+        for (Player p : players) {
             System.out.println(p);
         }
 
     }
 
 
+    public void runGameLoop() {
+        int count = 0;
+        boolean continueGame = true;
 
-    public void runGameLoop(){
+        while (continueGame == true) {
 
-        this.currentPlayer = players.get(0);
+            this.currentPlayer = players.get(count);
+            count += 1;
+            throwAndMove();
+            continueGame = ui.promptBinary("Fortsæt? (Y/N)");
+            if (count == players.size()) {
+                count = 0;
+            }
+        }
+    }
 
-        if(endsWithS()){
-            ui.displayMsg("Det er "+ currentPlayer.getName()+"' tur");
-        }else{
-            ui.displayMsg("Det er "+ currentPlayer.getName()+"'s tur");
+    private boolean endsWithS() {
+
+        int n = currentPlayer.getName().length();
+        char last = currentPlayer.getName().charAt(n - 1);
+        if (last == 's' || last == 'S') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void throwAndMove() {
+        if (endsWithS()) {
+            ui.displayMsg("Det er " + currentPlayer.getName() + "' tur");
+        } else {
+            ui.displayMsg("Det er " + currentPlayer.getName() + "'s tur");
         }
 
     }
 
-    private boolean endsWithS(){
+    public void landAndAct() {
 
-        int n = currentPlayer.getName().length();
-        char last = currentPlayer.getName().charAt(n-1);
-        if(last == 's' || last == 'S'){
-           return true;
-        }else {
-            return false;
-        }
     }
 
 
     public void endSession() {
         ArrayList<String> playerData = new ArrayList<>();
 
-        //serialiserer player objekterner
-        for(Player p: players){
+        //serialiserer player objekterne
+        for (Player p : players) {
             String s = p.toString();
             playerData.add(s);
         }
 
-       io.saveData(playerData, "data/playerData.csv", "Name, Score");
+        io.saveData(playerData, "data/playerData.csv", "Name, Score");
+        ui.displayMsg("Spillet er slut!");
     }
 }
