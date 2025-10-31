a3914f253472d28163059d03dc1495ebdc320f51
cph-dr147 <dana.albidani@gmail.com>
2025-10-31 10:25:30 +0100

    2

--- src/Game.java
+++ src/Game.java
@@ -15,42 +15,41 @@ public class Game {
     FileIO io = new FileIO();
 
 
-    public Game(String name, int maxPlayers){
+    public Game(String name, int maxPlayers) {
         this.name = name;
         this.maxPlayers = maxPlayers;
         players = new ArrayList<>();
     }
 
 
-    public void startSession(){
+    public void startSession() {
         ui.displayMsg("Welcome to the game.");
         ArrayList<String> data = io.readData("data/playerData.csv");
-        if(!data.isEmpty() && ui.promptBinary("Continue previously saved game? Y/N")){
-            for(String s : data){
-                String[] values =  s.split(",");//  "tess, 0"
+        if (!data.isEmpty() && ui.promptBinary("Continue previously saved game? Y/N")) {
+            for (String s : data) {
+                String[] values = s.split(",");//  "tess, 0"
                 int score = Integer.parseInt(values[1].trim());
-                createPlayer(values[0],score);
+                createPlayer(values[0], score);
             }
 
-        }else{
+        } else {
             registerPlayers();
         }
         displayPlayers();
     }
 
 
-
-    public void registerPlayers(){
+    public void registerPlayers() {
 
         int totalPlayers = ui.promptNumeric(" Tast antal deltagere: ");
-        if(totalPlayers <= maxPlayers && totalPlayers > 1){
+        if (totalPlayers <= maxPlayers && totalPlayers > 1) {
 
-            while(this.players.size() < totalPlayers) {
+            while (this.players.size() < totalPlayers) {
                 String playerName = ui.promptText("Tast spiller navn");
                 this.createPlayer(playerName, 0);
             }
             Collections.shuffle(players);
-        }else {
+        } else {
 
             ui.displayMsg("Eroor. Enter a number between 2 and 6");
             registerPlayers();
@@ -59,12 +58,13 @@ public class Game {
     }
 
 
-    private void createPlayer(String name, int score){
+    private void createPlayer(String name, int score) {
         Player p = new Player(name, score);
         players.add(p);
     }
-    public void displayPlayers(){
-        for(Player p:players){
+
+    public void displayPlayers() {
+        for (Player p : players) {
             System.out.println(p);
         }
 
@@ -74,17 +74,28 @@ public class Game {
         ArrayList<String> playerData = new ArrayList<>();
 
         //serialiserer player objekterner
-        for(Player p: players){
+        for (Player p : players) {
 
             String s = p.toString();
             playerData.add(s);
 
         }
-       io.saveData(playerData, "data/playerData.csv", "Name, Score");
+        io.saveData(playerData, "data/playerData.csv", "Name, Score");
     }
-   public void runGameLoop(){
-        currentPlayer = players.get(0);
-        ui.displayMsg("'It is " + currentPlayer.getName()+ "'s turn ");
 
+    public void runGameLoop() {
+        int count = 0;
+        boolean continueGame = true;
+        while (continueGame == true) {
+            currentPlayer = players.get(count);
+            count++;
+            ui.displayMsg("'It is " + currentPlayer.getName() + "'s turn ");
+            if (count >= players.size()) {
+                count = 0;
+
+            }
+
+
+        }
     }
 }
