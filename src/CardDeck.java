6007ee890dc5fc43b7710dcc18dd06ac233aeb07
AlteredProject <125555532+AlteredProject@users.noreply.github.com>
2025-11-03 10:46:50 +0100

    CardDeck klasse der laver og blander kort decket

---
+++ src/CardDeck.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+public class CardDeck {
+    private Card[] cards;
+    private int counter;
+
+    public CardDeck(String[] carddata) {
+        this.cards = new Card[carddata.length];
+        this.counter = 0;
+
+    }
+
+    public Card getNext() {
+        counter = (counter == cards.length) ? this.counter = 0 : this.counter++;
+        return cards[counter];
+    }
+
+
+    private void createCards(String[] data) {
+        for (int i = 0; i < data.length; i++) {
+            String[] values = data[i].split(",");
+            String message = values[0];
+            int income = Integer.parseInt(values[1]);
+            int cost = Integer.parseInt(values[2]);
+            String event = values[3];
+
+            Card c = new Card(message, income, cost, event);
+            cards[i] = c;
+
+            Collections.shuffle(Arrays.asList(cards));
+        }
+
+    }
+}
