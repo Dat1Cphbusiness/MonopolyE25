72927670097b38bdb4ecfc5ff14cef4a303f3b09
MathiasStenvall <cph-ms1422@stud.ek.dk>
2025-11-03 10:15:00 +0100

    Card & CardDeck classes.

---
+++ src/CardDeck.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+public class CardDeck {
+    private Card[] cards;
+    private int counter;
+
+    public CardDeck(Card[] cards) {
+        this.cards = new Card[cards.length];
+        this.counter = 0;
+    }
+    public Card getNext(){
+        if (counter >=  cards.length){
+            counter = 0;
+        } else {
+            counter++;
+        }
+        return cards[counter];
+    }
+
+    public void createCards(String[] data){
+        for (int i=1; i<data.length; i++){
+            String[] value = data[i].split(",");
+            String message = value[0].trim();
+            int income = value[1].trim();
+            int cost = value[2].trim();
+            String event = value[3].trim();
+            Card c = new Card(message, income, cost, event);
+            cards[i-1] = c;
+        }
+        Collections.shuffle(Arrays.asList(cards));
+    }
+}
