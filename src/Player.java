ade39f4ad9501afebc460d414241f925d98a699b
Peter Mærsk <petersondergaard1@gmail.com>
2025-11-04 12:11:10 +0100

    Peter og Gabriel har lavetr sprint7 player

--- src/Player.java
+++ src/Player.java
@@ -1,11 +1,19 @@
+import java.util.ArrayList;
+
 public class Player {
     private String name;
     private int balance;
     private int position;
-    
-    public Player(String name, int balance){
+    private ArrayList<Property> deeds;
+    ArrayList<Card> chanceCards;
+    boolean hasWildcard;
+    private int diceRoll;
+
+
+    public Player(String name, int balance, boolean hasWildcard){
         this.name = name;
         this.balance = balance;
+        this.hasWildcard = hasWildcard;
     }
 
     @Override
@@ -21,14 +29,17 @@ public class Player {
     public int updatePosition(int value){
 
         this.position =  this.position + value;
-
+        diceRoll = value;
         if(this.position > 40){
             this.position = this.position - 40;
             Bank bank = new Bank();
             bank.deposit(4000, this);
         }
-
         return this.position;
+    }
+
+    public int getDiceRoll(){
+        return diceRoll;
     }
 
     public int getBalance() {
@@ -42,5 +53,42 @@ public class Player {
 
     public void setPosition(int i) {
         this.position = i;
+    }
+
+   public boolean buyProperty(Property pf){
+    return true;
+    }
+
+    private boolean startPassed(){
+        return false;
+    }
+
+    public void moveToPosition(int value){
+        this.position = value;
+    }
+
+    public boolean imprison(){
+
+        moveToPosition(31);
+        return true;
+    }
+
+    public boolean freeFromprison(){
+      return false;
+    }
+
+    public boolean getWildcard() {
+        return hasWildcard;
+    }
+        public void setWildcard ( boolean value){
+            hasWildcard = value;
+
+    }
+    public boolean isHasWildcard() {
+        return hasWildcard;
+    }
+
+    public void setHasWildcard(boolean hasWildcard) {
+        this.hasWildcard = hasWildcard;
     }
 }
