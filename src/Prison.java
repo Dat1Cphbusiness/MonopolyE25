2858058021e14a6416b5fd359de958123f11ed44
Nali1234-lab <naliahmadi00@gmail.com>
2025-11-04 12:07:19 +0100

    sprint7-prison

--- src/Prison.java
+++ src/Prison.java
@@ -4,11 +4,34 @@ public class Prison extends Field {
     }
     @Override
     public String onAccept(Player p) {
+        p.setWildCard()= false;
+        String msg= "\n Du har kontakter hos politiet og løslades. ";
+
         return super.onAccept(p);
     }
 
     @Override
     public String onReject(Player p) {
+        p.imprison();
+        String msg= "\n Næste gang det bliver din tur," +
+        " kan du vælge at betale dig ud,´eller prøve at så et dobbletslag." +
+                " Når du hat siddet over i tre omgange er du løsladt";
+
         return super.onReject(p);
+
+    }
+    public String onLand(Player p) {
+        String msg =  super.onLand(p);
+        msg += "\n Du er arresteret, og skal i fængsel";
+        if(p.getWildCard() == true){
+            msg +="\n Vil du brug dit wildcard? (Y/N) ";
+        }else {
+          p.imprison();
+          msg += "\n Næste gang det bliver din tur," +
+                  " kan du vælge at betale dig ud,´eller prøve at så et dobbletslag." +
+                  " Når du hat siddet over i tre omgange er du løsladt";
+        }
+        return msg;
+
     }
 }
